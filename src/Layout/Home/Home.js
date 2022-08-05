d0950eca99d4350f677b05e0c41646a49f40a32b
Adam Clingmon <adamclingmon@gmail.com>
2022-08-05 15:27:22 -0700

    2nd commit

---
+++ src/Layout/Home/Home.js
@@ -0,0 +1,14 @@
+import react from "react";
+import CreateDeck from "../CreateDeck";
+import NotFound from "../NotFound";
+import DeckList from "./DeckList";
+import Header from "../Comp/Header";
+
+function home() {
+  return;
+  <div>
+    <Header />
+  </div>;
+}
+
+export default home;
---
+++ src/Layout/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "./Header";
+import NotFound from "./NotFound";
+
+function Layout() {
+  function home() {}
+}
+export default Layout;
