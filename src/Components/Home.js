ffde7a631d20651fd75d088ea1e23c0ae8b884a7
Adam Clingmon <adamclingmon@gmail.com>
2022-08-06 15:02:11 -0700

    home.js changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,34 @@
 import react, { useEffect } from "react";
 import CreateDeck from "./CreateDeck";
-import NotFound from "./NotFound";
-import DeckList from "./DeckList";
 import Header from "../Layout/Comp/Header";
+import {
+  listDecks,
+  deleteDeck,
+  readDeck,
+  updateDeck,
+} from "../utils/api/index";
+import CardView from "./CardView";
+import Study from "./Study";
 
 function home() {
-  const [decks, setDecks] = useState(DeckData);
+  const [decks, setDecks] = useState(decks);
   useEffect(() => {
-    async function getDecks()
-  })
-  const deleteDeck = (indexToDelete) => {
-    setDecks((curDeck) => curDeck.filter((post, index) => index !== indexToDelete))
-  }
+    async function getDecks() {
+      const dataFromAPI = await listDecks();
+      setDecks(dataFromAPI);
+    }
+    getDecks();
+  }, []);
+  // useEffect(() => {
+  //   async function
+  // })
 
   return;
   <div>
     <Header />
+    <CreateDeck />
+    <Study />
+    <button />
   </div>;
 }
 
